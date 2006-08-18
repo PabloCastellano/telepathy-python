e59592002d6aa465c2dbf44a539459e2409e1b70
Simon McVittie <simon.mcvittie@collabora.co.uk>
2006-08-18 17:40:05 +0000

    Remove unnecessary import of gobject
    20060818174005-53eee-b30eebbf0f6745c8bf86b04fd56843befcfec893.gz

--- telepathy/client/channel.py
+++ telepathy/client/channel.py
@@ -18,7 +18,6 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 import dbus
-import gobject
 
 from telepathy.client.interfacefactory import (InterfaceFactory,
                                                default_error_handler)
