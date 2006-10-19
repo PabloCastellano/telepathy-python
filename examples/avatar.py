73806488557b5f828a03b765d4028059f9ceea1c
Simon McVittie <simon.mcvittie@collabora.co.uk>
2006-10-19 14:31:49 +0000

    examples/avatar.py: pull in some avatar test code from Daf
    20061019143149-53eee-01197c608e4a263affc91c7b7f39ebf5841f44f4.gz

--- examples/avatar.py
+++ examples/avatar.py
@@ -4,37 +4,41 @@ Telepathy example which requests the avatar for the user's own handle and
 displays it in a Gtk window.
 """
 
+import dbus
 import dbus.glib
 import gtk
 import sys
+
+from pprint import pprint
 
 from telepathy.constants import CONNECTION_STATUS_CONNECTED
 from telepathy.interfaces import (
     CONN_MGR_INTERFACE, CONN_INTERFACE, CONN_INTERFACE_AVATARS)
 import telepathy.client
 
-def parse_account(s):
-    lines = s.splitlines()
-    pairs = []
-
-    for line in lines:
-        k, v = line.split(':', 1)
-        k = k.strip()
-        v = v.strip()
-        pairs.append((k, v))
-
-    return dict(pairs)
+from account import read_account, connect
 
 def window_closed_cb(window):
     gtk.main_quit()
 
 def status_changed_cb(state, reason):
+    print 'status changed'
+
     if state != CONNECTION_STATUS_CONNECTED:
         return
 
+    print 'connected'
     handle = conn[CONN_INTERFACE].GetSelfHandle()
     tokens = conn[CONN_INTERFACE_AVATARS].GetAvatarTokens([handle])
     print 'token:', tokens[0]
+
+    if len(sys.argv) > 2:
+        avatar = file('avatar.png').read()
+        new_token = conn[CONN_INTERFACE_AVATARS].SetAvatar(avatar, 'image/png')
+        print 'new token:', new_token
+        gtk.main_quit()
+        return
+
     image, mime = conn[CONN_INTERFACE_AVATARS].RequestAvatar(handle)
     image = ''.join(chr(i) for i in image)
 
@@ -49,28 +53,16 @@ def status_changed_cb(state, reason):
     window.connect('destroy', gtk.main_quit)
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        account_file = sys.argv[1]
-    else:
-        account_file = 'account'
+    account_file = sys.argv[1]
+    manager, protocol, account = read_account(account_file)
+    conn = connect(manager, protocol, account)
 
-    reg = telepathy.client.ManagerRegistry()
-    reg.LoadManagers()
+    # XXX: hack!
+    conn._valid_interfaces = [CONN_INTERFACE, CONN_INTERFACE_AVATARS]
+    conn[CONN_INTERFACE].connect_to_signal('StatusChanged', status_changed_cb)
 
-    account = parse_account(file(account_file).read())
-    manager = account['manager']
-    protocol = account['protocol']
-    del account['manager']
-    del account['protocol']
-
-    mgr = reg.GetManager(manager)
-    conn_bus_name, conn_object_path = mgr[CONN_MGR_INTERFACE].Connect(
-        protocol, account)
-    def on_ready(conn):
-        conn[CONN_INTERFACE].connect_to_signal('StatusChanged', status_changed_cb)
-    conn = telepathy.client.Connection(conn_bus_name, conn_object_path,
-                                       ready_handler=on_ready)
-
+    print 'connecting'
+    conn[CONN_INTERFACE].Connect()
     gtk.main()
     conn[CONN_INTERFACE].Disconnect()
 
