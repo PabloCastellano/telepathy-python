7884b5413e253bf0c8230a70ddf58d11ae907fff
Simon McVittie <simon.mcvittie@collabora.co.uk>
2006-11-24 14:24:13 +0000

    setup.py: get version from telepathy.__version__
    20061124142413-53eee-5e996da9add616bd2de666a5535a49fd58aa35af.gz

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 
 from distutils.core import setup
+from telepathy import __version__
 
 setup(
     name='telepathy-python',
-    version='0.13.3',
+    version=__version__,
     packages=[
         'telepathy',
         'telepathy.client',
