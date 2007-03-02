e3ed8f82968899daef7e48732f41fd8e3bd86a1d
Simon McVittie <simon.mcvittie@collabora.co.uk>
2007-03-02 15:01:16 +0000

    Add infrastructure to generate dbus.service.Interface from the spec
    20070302150116-53eee-9903d0f4f975933e16cd7b589d29f1ec9ae7828a.gz

--- setup.py
+++ setup.py
@@ -15,13 +15,78 @@ execfile(os.path.join(os.curdir, 'telepathy', '_version.py'),
          globals(), loc)
 __version__ = loc['__version__']
 
+
 def ls_spec_xml():
     names = os.listdir(os.path.join(os.curdir, 'spec'))
     return [os.path.join(os.curdir, 'spec', name) for name in names]
 
+def ls_spec_basenames():
+    names = os.listdir(os.path.join(os.curdir, 'spec'))
+    for name in names:
+        assert name.endswith('.xml')
+    return [name[:-4] for name in names]
+
 
 XSLTPROC = ['xsltproc', '--nonet', '--novalid', '--xinclude']
 ALL_SPEC_XML = ls_spec_xml()
+ALL_SPEC_BASENAMES = ls_spec_basenames()
+
+class build_empty_py(build_py):
+    def finalize_options(self):
+        build_py.finalize_options(self)
+        self.packages = []
+        self.py_modules = ['telepathy._generated']
+        self.package_data = []
+        self.data_files = []
+
+    def find_modules(self):
+        return [('telepathy._generated', '__init__', '')]
+
+    def build_module(self, module, module_file, package):
+        # "module_file" is actually the empty string
+        package = package.split('.')
+        outfile = self.get_module_outfile(self.build_lib, package, module)
+        dir = os.path.dirname(outfile)
+        log.info('Generating empty %s' % outfile)
+        self.mkpath(dir)
+        if not self.dry_run:
+            f = file(outfile, 'w')
+            f.write('# Placeholder for package')
+            f.close()
+
+class build_gen_py_ifaces(build_py):
+    def finalize_options(self):
+        build_py.finalize_options(self)
+        self.packages = []
+        self.py_modules = ['telepathy._generated.%s' % name
+                           for name in ALL_SPEC_BASENAMES]
+        self.package_data = []
+        self.data_files = []
+        self.stylesheet = os.path.join(os.curdir, 'tools',
+                                       'spec-to-python.xsl')
+
+    def find_modules(self):
+        return [('telepathy._generated', name,
+                 os.path.join(os.curdir, 'spec', '%s.xml' % name))
+                for name in ALL_SPEC_BASENAMES]
+
+    def build_module(self, module, module_file, package):
+        # "module_file" is actually the XML
+        package = package.split('.')
+        outfile = self.get_module_outfile(self.build_lib, package, module)
+        dir = os.path.dirname(outfile)
+        self.mkpath(dir)
+        return self.run_xsl(module_file, outfile)
+
+    def run_xsl(self, xml, outfile):
+        if newer_group([xml, self.stylesheet], outfile):
+            command = XSLTPROC + ['-o', outfile,
+                                  self.stylesheet,
+                                  xml]
+            log.info('Generating %s from %s using %s' % (outfile, xml,
+                                                         self.stylesheet))
+            spawn(command, dry_run=self.dry_run)
+
 
 class build_gen_py(build_py):
     def finalize_options(self):
@@ -60,10 +125,15 @@ class build_gen_py(build_py):
 
 class build(_build):
     sub_commands = (_build.sub_commands
-                    + [('build_gen_py', (lambda self: True))])
+                    + [('build_gen_py', (lambda self: True)),
+                       ('build_gen_py_ifaces', (lambda self: True)),
+                       ('build_empty_py', (lambda self: True))])
 
 setup(
-    cmdclass={'build': build, 'build_gen_py': build_gen_py},
+    cmdclass={'build': build,
+              'build_gen_py': build_gen_py,
+              'build_empty_py': build_empty_py,
+              'build_gen_py_ifaces': build_gen_py_ifaces},
     name='telepathy-python',
     version=__version__,
     packages=[
