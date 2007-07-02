38b34d99cd63a70e3bb93a55685c3296fb837164
Simon McVittie <simon.mcvittie@collabora.co.uk>
2007-07-02 14:51:58 +0000

    telepathy.client.interfacefactory: errors go out through the logging framework
    20070702145158-53eee-f6d704804b833fb960ecda22bc11825d9f42aa1c.gz

--- telepathy/client/interfacefactory.py
+++ telepathy/client/interfacefactory.py
@@ -17,12 +17,15 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-from sys import stderr
-
 import dbus
+import logging
+
+
+logger = logging.getLogger('telepathy.client.interfacefactory')
 
 def default_error_handler(exception):
-    stderr.write('Exception from asynchronous method call:\n%s\n' % exception)
+    logging.basicConfig()
+    logger.warning('Exception from asynchronous method call:\n%s', exception)
 
 class InterfaceFactory(object):
     def __init__(self, dbus_object, default_interface=None):
