5487b662f43c5cda757e7275a14116aa64538d82
Louis-Francis Ratte-Boulianne <lfrb@lfrb-desktop.no-domain-set.bellcanada>
2009-06-26 18:33:23 -0400

    Added FileTransfer channel

--- src/server/channel.py
+++ src/server/channel.py
@@ -31,6 +31,7 @@ from telepathy.interfaces import (CHANNEL_INTERFACE,
                                   CHANNEL_INTERFACE_HOLD,
                                   CHANNEL_INTERFACE_PASSWORD,
                                   CHANNEL_TYPE_CONTACT_LIST,
+                                  CHANNEL_TYPE_FILE_TRANSFER,
                                   CHANNEL_TYPE_ROOM_LIST,
                                   CHANNEL_TYPE_STREAMED_MEDIA,
                                   CHANNEL_TYPE_TEXT,
@@ -142,6 +143,22 @@ from telepathy._generated.Channel_Type_Contact_List \
 
 class ChannelTypeContactList(Channel, _ChannelTypeContactListIface):
     __doc__ = _ChannelTypeContactListIface.__doc__
+
+    def __init__(self, connection, manager, props):
+        """
+        Initialise the channel.
+
+        Parameters:
+        connection - the parent Telepathy Connection object
+        """
+        Channel.__init__(self, connection, manager, props)
+
+
+from telepathy._generated.Channel_Type_File_Transfer \
+        import ChannelTypeFileTransfer as _ChannelTypeFileTransferIface
+
+class ChannelTypeFileTransfer(Channel, _ChannelTypeFileTransferIface):
+    __doc__ = _ChannelTypeFileTransferIface.__doc__
 
     def __init__(self, connection, manager, props):
         """
